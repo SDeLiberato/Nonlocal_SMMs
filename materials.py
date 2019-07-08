84b8c4cd0b04369ec20a82588c6da095feee8626
Christopher Gubbin <c.r.gubbin@soton.ac.uk>
2019-07-08 13:08:47 +0100

    Refactored material properties code and functions into a self-contained class

--- materials.py
+++ materials.py
@@ -1,104 +1,137 @@
-__all__ = ['epsilon_f', 'mu_f', 'properties']
+__all__ = ['media', 'properties']
 
 import numpy as np
 from scipy.constants import speed_of_light
 
-def eps_1_osc(wn, wlo,wto,eps_inf,gam):
-    """ Returns the dielectric function evaluated at frequencies wn, assuming a
-    1 oscillator Lorentz model of the dielectric. For materials whose phonon
-    frequencies are set to nil returns eps_inf
+class media:
 
-    Parameters
-    ----------
+    def __init__(self, name, props, wn):
+        """ Initiates a material instance with name specified by a string and properties by
+        a dict
 
-    wn : float or array
-        the frequencies to probe
+        Parameters
+        ----------
 
-    wlo : float
-        longitudinal optic phonon frequency
+        name : string
+            name of the material comprising the layer
 
-    wto : float
-        transverse optic phonon frequency
+        props : dict
+            dict containing the material properties
 
-    eps_inf : float
-        high-frequency dielectric constant
+        wn : float or array
+            float or array containing the probe frequencies
+        """
+        self._name = name
+        for k, v in props.items():
+            setattr(self, '_'+k, v)
+        self._wn = wn
+        self._eps = self.epsilon_tensor(wn)
+        self._mu = self.mu_tensor(wn)
+        self._eps_inf = np.diag(
+            [self._eps_inf_pe, self._eps_inf_pe, self._eps_inf_pa]
+             )
+        self._alpha =  np.diag(
+            [np.sqrt(self._eps_0_pe-self._eps_inf_pe)*self._wto_pe,
+            np.sqrt(self._eps_0_pe-self._eps_inf_pe)*self._wto_pe,
+            np.sqrt(self._eps_0_pa-self._eps_inf_pa)*self._wto_pa]
+            )
 
-    gamma : float
-        damping rate
 
-    Returns
-    -------
+    def eps_1_osc(self, wn, orientation='pe'):
+        """ Returns the dielectric function evaluated at frequencies wn, assuming a
+        1 oscillator Lorentz model of the dielectric. For materials whose phonon
+        frequencies are set to nil returns eps_inf
 
-    eps : float or array
-        dielectric constant evaluated at the input frequencies wn
+        Parameters
+        ----------
 
-    """
-    eps =  eps_inf*(wlo**2 - wn*(wn+1j*gam))/(wto**2 - wn*(wn+1j*gam))
-    return eps
+        wn : float or array
+            the frequencies to probe
 
-def epsilon_f(properties, wn):
-    """ Returns the permittivity tensor evaluated at frequencies wn, currently
-    assuming a 1 oscillator Lorentz model of the dielectric and that the material
-    is orientated so it's c-axis is parallel to the z-direction. 
+        orientation : string
+            either pe or pa, determines whether the function is calculated parallel or
+            perpendicular to the crystal axis
 
-    Parameters
-    ----------
+        Returns
+        -------
 
-    properties : dict
-        contains the material properties to utilise in the calculation
+        eps : float or array
+            dielectric constant evaluated at the input frequencies wn
 
-    wn : float or array
-        the frequencies to probe
+        """
+        if orientation == 'pe':
+            wlo, wto, eps_inf, gam = (
+                self._wlo_pe, self._wto_pe,
+                self._eps_inf_pe, self._gamma
+                )
+        elif orientation == 'pa':
+            wlo, wto, eps_inf, gam = (
+                self._wlo_pa, self._wto_pa,
+                self._eps_inf_pa, self._gamma
+                )
 
-    Returns
-    -------
+        eps =  eps_inf*(wlo**2 - wn*(wn+1j*gam))/(wto**2 - wn*(wn+1j*gam))
 
-    eps : array
-        permittivity tensor evaluated at the input frequencies wn assuming the
-        crystal c-axis is parallel to the third dimension
+        return eps
 
-    """
-    wto_pe = properties['wto_pe']
-    wto_pa = properties['wto_pa']
-    wlo_pe = properties['wlo_pa']
-    wlo_pa = properties['wlo_pa']
-    eps_inf_pe = properties['eps_inf_pe']
-    eps_inf_pa = properties['eps_inf_pa']
-    gamma = properties['gamma']
-    eps = np.zeros((len(wn),3,3),dtype=complex)
-    eps[:,0,0] = eps_1_osc(wn,wlo_pe,wto_pe,eps_inf_pe,gamma)
-    eps[:,1,1] = eps_1_osc(wn,wlo_pe,wto_pe,eps_inf_pe,gamma)
-    eps[:,2,2] = eps_1_osc(wn,wlo_pa,wto_pa,eps_inf_pa,gamma)
-    
-    return eps
+    def epsilon_tensor(self, wn):
+        """ Returns the permittivity tensor evaluated at frequencies wn, currently
+        assuming a 1 oscillator Lorentz model of the dielectric and that the material
+        is orientated so it's c-axis is parallel to the z-direction.
 
-def mu_f(properties, wn):
-    """ Returns the permeability tensor evaluated at frequencies wn, currently
-    assuming non-magnetic media.
+        Parameters
+        ----------
 
-    Parameters
-    ----------
+        properties : dict
+            contains the material properties to utilise in the calculation
 
-    properties : dict
-        contains the material properties to utilise in the calculation
+        wn : float or array
+            the frequencies to probe
 
-    wn : float or array
-        the frequencies to probe
+        Returns
+        -------
 
-    Returns
-    -------
+        eps : array
+            permittivity tensor evaluated at the input frequencies wn assuming the
+            crystal c-axis is parallel to the third dimension
 
-    mu : array
-        permeability tensor evaluated at the input frequencies wn assuming the
-        crystal c-axis is parallel to the third dimension
+        """
 
-    """
-    mu = np.zeros((3,3),dtype=complex)
-    mu[0,0] = 1
-    mu[1,1] = 1
-    mu[2,2] = 1
-    
-    return mu  
+        eps = np.zeros((len(wn),3,3),dtype=complex)
+        eps[:,0,0] = self.eps_1_osc(wn, 'pe')
+        eps[:,1,1] = self.eps_1_osc(wn, 'pe')
+        eps[:,2,2] = self.eps_1_osc(wn, 'pa')
+
+        return eps
+
+    def mu_tensor(self, wn):
+        """ Returns the permeability tensor evaluated at frequencies wn, currently
+        assuming non-magnetic media.
+
+        Parameters
+        ----------
+
+        properties : dict
+            contains the material properties to utilise in the calculation
+
+        wn : float or array
+            the frequencies to probe
+
+        Returns
+        -------
+
+        mu : array
+            permeability tensor evaluated at the input frequencies wn assuming the
+            crystal c-axis is parallel to the third dimension
+
+        """
+        mu = np.zeros((3,3),dtype=complex)
+        mu[0,0] = 1
+        mu[1,1] = 1
+        mu[2,2] = 1
+
+        return mu
+
 
 def properties(mat):
     """ Returns a dict containing the relevant properties of the material specified
@@ -115,7 +148,7 @@ def properties(mat):
 
     props : dict
         Contains the relevant parameters corresponding to the string mat
-    
+
 
     References
     ----------
@@ -142,7 +175,7 @@ def properties(mat):
             props['gamma'] = 0
         if mat == 'SiC3C':
             props['beta_c'] = 1j*4e3/speed_of_light
-            props['beta_l'] = 0.1j*9e3/speed_of_light
+            props['beta_l'] = 1j*9e3/speed_of_light
             props['beta_t'] = 1j*2e3/speed_of_light
             props['rho'] = 3.21
             props['eps_inf_pa'] = 6.52
@@ -152,7 +185,7 @@ def properties(mat):
             props['wto_pa'] = 797.5
             props['wto_pe'] = 797.5
             props['gamma'] = 4
-        
+
     props['wlo_pa'] = props['wto_pa']*np.sqrt(props['eps_0_pa']/props['eps_inf_pa'])
     props['wlo_pe'] = props['wto_pe']*np.sqrt(props['eps_0_pe']/props['eps_inf_pe'])
     return props
