f564841c5d51006c7c1165cb6dd1df9126fa3166
Christopher Gubbin <c.r.gubbin@soton.ac.uk>
2019-07-09 10:01:18 +0100

    Refactored equations according to documentation and tidied syntax.

--- dispersion.py
+++ dispersion.py
@@ -4,6 +4,7 @@ import numpy as np
 from itertools import product
 from materials import media, properties
 
+
 def layer_dispersion(kx, wn, material, ang=None):
     """ Calculates the dispersion of the photonic and longitudinal and transverse
     phonon eigenmodes of an anisotropic medium, describable by a diagonal
@@ -29,12 +30,12 @@ def layer_dispersion(kx, wn, material, ang=None):
     -------
 
     eigs : array
-        Values representing calculated out-of-plane wavevectors of the eigenmodes
-        of the layer.
+        Values representing calculated out-of-plane wavevectors of the
+        eigenmodes of the layer.
 
     fields : array
-        Values representing the calculated field components corresponding to the
-        eigenmodes of the layer
+        Values representing the calculated field components corresponding to
+        the eigenmodes of the layer
 
 
     References
@@ -44,90 +45,105 @@ def layer_dispersion(kx, wn, material, ang=None):
     detailed at " "
     """
 
-    nkx = len(kx)
-    ### Checks for non-array inputs and converts to numpy arrays
+    # Checks for non-array inputs and converts to numpy arrays
     if type(kx) != np.ndarray:
         kx = np.array(kx)
     if type(wn) != np.ndarray:
         wn = np.array(wn)
 
-    ### Checks for equal length inputs, assumed to correspond to a cut through
-    ### (kx, wn) space
-    if len(kx)==len(wn):
-        arr = np.transpose(np.array([wn,kx]))
+    # Checks for equal length inputs, assumed to correspond to a cut through
+    # (kx, wn) space
+    if len(kx) == len(wn):
+        arr = np.transpose(np.array([wn, kx]))
     else:
         arrays = [(wn), (kx)]
         arr = np.array(list(product(*arrays)))
 
-    zeta = arr[:,1]/arr[:,0]
-    ### Checks for an input angle, if present recalculates zeta
+    zeta = arr[:, 1]/arr[:, 0]
+    # Checks for an input angle, if present recalculates zeta
     if ang:
         zeta[:] = np.sin(ang*np.pi/180)
-    mu1 = 1
 
-
-    ### Fetches material properties
+    # Fetches material properties
     props = properties(material)
 
     layer = media(material, props, wn)
 
-    ### Creates an array of len(zeta) 5 x 5 identity matrices
+    # Creates an array of len(zeta) 5 x 5 identity matrices
     It = np.identity(5)
-    It = np.swapaxes(np.broadcast_to(It[...,None], (5,5,len(zeta))), 0, 2)
-    ### Creates an array of len(zeta) 5 x 5 null matrices
+    It = np.swapaxes(np.broadcast_to(It[..., None], (5, 5, len(zeta))), 0, 2)
+    # Creates an array of len(zeta) 5 x 5 null matrices
     Zt = np.zeros_like(It)
-    ### Creates an array of len(zeta) 5 x 5 null matrices to be filled
+    # Creates an array of len(zeta) 5 x 5 null matrices to be filled
     At = np.zeros_like(It, dtype=complex)
     Bt = np.zeros_like(It, dtype=complex)
 
-    At[:,0,0] = (zeta**2-layer._eps_inf[2,2]*layer._mu[0,0])/layer._eps_inf[2,2]
-    At[:,1,1] = -layer._mu[0,0]
-    At[:,2,2] = 2/layer._beta_c**2
-    At[:,2,2] = -2/layer._beta_c**2
-    At[:,3,3] = 2/layer._beta_c**2
-    At[:,4,4] = 1/layer._beta_l**2
-    At[:,3,3] = -2/layer._beta_c**2
-    At[:,4,4] = -1/layer._beta_l**2
+    At[:, 0, 0] = (
+        layer._eps_inf[2, 2]/(zeta**2-layer._eps_inf[2, 2]*layer._mu[1, 1])
+        )
+    At[:, 1, 1] = (-1/layer._mu[0, 0])
+    At[:, 2, 2] = (layer._beta_c**2/2)
+    At[:, 3, 3] = (layer._beta_c**2/2)
+    At[:, 4, 4] = (layer._beta_l**2)
 
-    Bt[:,0,4] = - zeta*np.sqrt(4*np.pi)*layer._alpha[2,2]/layer._eps_inf[2,2]
-    Bt[:,2,4] = - zeta*(layer._beta_l**2-2*layer._beta_t**2+layer._beta_c**2/2)*2/layer._beta_c**2
-    Bt[:,4,0] = - zeta*layer._alpha[2,2]/np.sqrt(4*np.pi)/(zeta**2-layer._eps_inf[2,2]*layer._mu[1,1])/layer._beta_l**2/arr[:,0]**2
-    Bt[:,4,0] =  zeta*layer._alpha[2,2]/np.sqrt(4*np.pi)/(zeta**2-layer._eps_inf[2,2]*layer._mu[1,1])/layer._beta_l**2/arr[:,0]**2
-    Bt[:,4,2] = - zeta*(layer._beta_l**2-2*layer._beta_t**2+layer._beta_c**2/2)/layer._beta_l**2
+    Bt[:, 0, 4] = (
+        zeta*np.sqrt(4*np.pi)*layer._alpha[2, 2]*layer._mu[1, 1]
+        / (zeta**2-layer._eps_inf[2, 2]*layer._mu[1, 1])
+        )
+    Bt[:, 2, 4] = zeta*(layer._beta_l**2-2*layer._beta_t**2+layer._beta_c**2/2)
+    Bt[:, 4, 0] = (
+        layer._alpha[2, 2]/np.sqrt(4*np.pi)*zeta
+        / (zeta**2-layer._eps_inf[2, 2]*layer._mu[1, 1])/arr[:, 0]**2
+        )
+    Bt[:, 4, 2] = zeta*(layer._beta_l**2-2*layer._beta_t**2+layer._beta_c**2/2)
 
     Ct = np.zeros_like(It, dtype=complex)
-    Ct[:,0,0] = layer._eps_inf[0,0]
-    Ct[:,0,2] = np.sqrt(4*np.pi)*layer._alpha[0,0]
-    Ct[:,1,1] = layer._eps_inf[1,1] - zeta**2/layer._mu[2,2]
-    Ct[:,1,3] = np.sqrt(4*np.pi)*layer._alpha[1,1]
-    Ct[:,2,0] = layer._alpha[0,0]/np.sqrt(4*np.pi)/arr[:,0]**2
-    Ct[:,2,2] = 1 + 1j*layer._gamma/arr[:,0] - layer._wto_pe**2/arr[:,0]**2 + zeta**2*layer._beta_l**2
-    Ct[:,2,2] = 1 + 1j*layer._gamma/arr[:,0] - layer._wto_pe**2/arr[:,0]**2 - zeta**2*layer._beta_l**2
 
-    Ct[:,3,1] = layer._alpha[1,1]/np.sqrt(4*np.pi)/arr[:,0]**2
-    Ct[:,3,3] = 1 + 1j*layer._gamma/arr[:,0] - layer._wto_pe**2/arr[:,0]**2 + zeta**2*layer._beta_c**2/2
-    Ct[:,4,4] = 1 + 1j*layer._gamma/arr[:,0] - layer._wto_pa**2/arr[:,0]**2 + zeta**2*layer._beta_c**2/2
-    Ct[:,3,3] = 1 + 1j*layer._gamma/arr[:,0] - layer._wto_pe**2/arr[:,0]**2 - zeta**2*layer._beta_c**2/2
-    Ct[:,4,4] = 1 + 1j*layer._gamma/arr[:,0] - layer._wto_pa**2/arr[:,0]**2 - zeta**2*layer._beta_c**2/2
+    Ct[:, 0, 0] = layer._eps_inf[0, 0]
+    Ct[:, 0, 2] = np.sqrt(4*np.pi)*layer._alpha[0, 0]
 
-    M1 = -np.concatenate((
-        np.hstack((Zt, It)),
-        np.hstack((At, Bt))
-        ), axis=2)
+    Ct[:, 1, 1] = layer._eps_inf[1, 1] - zeta**2/layer._mu[2, 2]
+    Ct[:, 1, 3] = np.sqrt(4*np.pi)*layer._alpha[1, 1]
+
+    Ct[:, 2, 0] = layer._alpha[0, 0]/np.sqrt(4*np.pi)/arr[:, 0]**2
+    Ct[:, 2, 2] = (
+        1 + 1j*layer._gamma/arr[:, 0]
+        - layer._wto_pe**2/arr[:, 0]**2 + zeta**2*layer._beta_l**2
+        )
+
+    Ct[:, 3, 1] = layer._alpha[1, 1]/np.sqrt(4*np.pi)/arr[:, 0]**2
+    Ct[:, 3, 3] = (
+        1 + 1j*layer._gamma/arr[:, 0]
+        - layer._wto_pe**2/arr[:, 0]**2 + zeta**2*layer._beta_c**2/2
+        )
+    Ct[:, 4, 4] = (
+        1 + 1j*layer._gamma/arr[:, 0]
+        - layer._wto_pa**2/arr[:, 0]**2 + zeta**2*layer._beta_c**2/2
+        + layer._alpha[2, 2]**2*layer._mu[1, 1]/arr[:, 0]**2
+        / (zeta**2-layer._eps_inf[2, 2]*layer._mu[1, 1])
+        )
+
+    M1 = -np.linalg.inv(np.concatenate((
+        np.hstack((Bt, At)),
+        np.hstack((It, Zt))
+        ), axis=2))
+
     M2 = np.concatenate((
         np.hstack((Ct, Zt)),
         np.hstack((Zt, -It))
         ), axis=2)
-    Mf = np.matmul(M1,M2)
+
+    Mf = np.matmul(M1, M2)
 
     eigs, fields = np.linalg.eig(Mf)
 
-    ### Order the calculated eigenvectors
-    fields[np.arange(np.shape(eigs)[0])[:,np.newaxis], np.argsort(eigs), :]
-    eigs = eigs[np.arange(np.shape(eigs)[0])[:,np.newaxis], np.argsort(eigs)]
-    ### Reshape into a useable array
-    eigs = eigs[:,:].reshape((len(kx), len(wn),10), order='F')
-    fields = fields[:,:].reshape((len(kx), len(wn),10, 10), order='F')
+    # Order the calculated eigenvectors
+    index = np.argsort(eigs)
+    eigs = eigs[np.arange(np.shape(eigs)[0])[:, np.newaxis], index]
+    fields = fields[np.arange(np.shape(eigs)[0])[:, np.newaxis], :, index]
+
+    # Reshape into a useable array
+    eigs = eigs[:, :].reshape((len(kx), len(wn), 10), order='F')
+    fields = fields[:, :].reshape((len(kx), len(wn), 10, 10), order='F')
+
     return eigs, fields
-
-
