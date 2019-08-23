b5121079e35ba92111607b62fb8d3c0637cc5454
Christopher Gubbin <c.r.gubbin@soton.ac.uk>
2019-08-23 16:44:26 +0100

    Added basic interface matrix calculation, works with angle input not kx

--- propagation.py
+++ propagation.py
@@ -1,4 +1,4 @@
-__all__ = ['prop_mat']
+__all__ = ['prop_mat', 'inter_mat']
 
 import numpy as np
 
@@ -38,3 +38,69 @@ def prop_mat(wn, eigs, thickness):
         prop_mat[:, idx, idx] = diag_vec[:, idx]
 
     return prop_mat
+
+
+def inter_mat(wn, eigs, mat, fields, ang=None, kx=None):
+    """ Calculates the interface matrix given that layer's eigenvalues
+    corresponding to the out-of-plane wavevector and eigenvectors
+
+    Parameters
+    ----------
+
+    wn : 1d array
+        a 1d array, containing the probing wavenumbers
+
+    eigs : array
+        a 2d array of dimensions (len(wn) x 10) where 10 is the number of
+        modes in the problem. This corresponds to the out of plane wavevector
+        divided by the wavenumber
+
+    fields : array
+        the array of sorted fields
+
+
+    Returns
+    -------
+
+    inter_mat : array
+        a matrix of size (len(wn)x10x10) containing the propagation factors for
+        each eigenmode
+    """
+
+    zeta = np.zeros_like(wn)
+    zeta[:] = np.sin(ang*np.pi/180)
+
+    inter_mat = np.zeros((len(wn), 10, 10), dtype=complex)
+
+    # In-plane electric field (x)
+    inter_mat[:, 0, :] = fields[:, :, 3]
+    # In-plane electric field (y)
+    inter_mat[:, 1, :] = fields[:, :, 4]
+
+    # In-plane magnetic field (x)
+    inter_mat[:, 2, :] = fields[:, :, 0]
+    # In-plane magnetic field (y)
+    inter_mat[:, 3, :] = fields[:, :, 1]
+
+    # In-plane polarisation field (x)
+    inter_mat[:, 4, :] = fields[:, :, 6]
+    # In-plane polarisation field (y)
+    inter_mat[:, 5, :] = fields[:, :, 7]
+    #  In-plane polarisation field (y)
+    inter_mat[:, 6, :] = fields[:, :, 8]
+
+    # In-plane stress tensor (x)
+    inter_mat[:, 7, :] = 0.5*mat._beta_c**2*(
+        eigs*fields[:, :, 9]+zeta*fields[:, :, 11]
+        )
+
+    # In-plane stress tensor (y)
+    inter_mat[:, 8, :] = 0.5*mat._beta_c**2*(eigs*fields[:, :, 10])
+
+    # In-plane stress tensor (z)
+    inter_mat[:, 9, :] = (
+        0.5*mat._beta_l**2*eigs*fields[:, :, 11]
+        + (mat._beta_l**2 - 2*mat._beta_t**2)*(zeta*fields[:, :, 9])
+    )
+
+    return inter_mat
