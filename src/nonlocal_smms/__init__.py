8d91707beb8f3873ab89d4a9be0bb2a771a38720
Christopher Gubbin <c.r.gubbin@soton.ac.uk>
2020-02-01 19:47:21 +0000

    Documentation

--- src/nonlocal_smms/__init__.py
+++ src/nonlocal_smms/__init__.py
@@ -1,3 +1,7 @@
-# src/nonlocal_smms/__init__.py
+"""Module applying the nonlocal scattering matrix method.
+
+This method is theoretically described in ... Here it is applied to
+the calculation of reflectance and transmittance of planar heterostructures.
+"""
 
 __version__ = "0.1.0"
