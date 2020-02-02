b14e43242fc1530448a05ee199902aaa406ef032
Christopher Gubbin <c.r.gubbin@soton.ac.uk>
2020-02-02 18:10:52 +0000

    Updated plotting function to accept a pre-created figure and axis as inputs

--- src/nonlocal_smms/plotting.py
+++ src/nonlocal_smms/plotting.py
@@ -3,11 +3,20 @@
 
 __all__ = ["RefPlot"]
 
+from typing import Any
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 # flake8: noqa: W605
-def RefPlot(wavenumber: np.ndarray, wavevector: np.ndarray, values: np.ndarray) -> None:
+def RefPlot(
+    wavenumber: np.ndarray,
+    wavevector: np.ndarray,
+    values: np.ndarray,
+    fig: Any,
+    ax: Any,
+    cax: bool = False,
+) -> None:
     """A helper function to generate a reflection map.
 
     Takes input wavenumbers, wavevectors and values, typically reflectance or transmittance
@@ -24,25 +33,19 @@ def RefPlot(wavenumber: np.ndarray, wavevector: np.ndarray, values: np.ndarray) 
 
     """
     X, Y = np.meshgrid(wavevector, wavenumber)
-    fig, axes = plt.subplots(
-        nrows=1, ncols=len(values), figsize=(18, 9), sharex=True, sharey=True
-    )
-    for idx, val in enumerate(values):
-        ax = axes[idx]
-        im = ax.pcolor(X, Y, val, cmap="Reds_r", vmin=0.5, vmax=1)
-
-        ax.plot(wavevector, wavevector, "r-")
-        ax.set_ylim([min(wavenumber), max(wavenumber)])
-        ax.set_xlabel("Rescaled in-plane-wavevector $K = k_x c/\omega_{\mathrm{L}1}$")
-        ax.set_ylabel("Rescaled frequency $\Omega = \omega/\omega_{\mathrm{L}1}$")
-        ax.set_title("Reflectance")
-        ax.spines["top"].set_visible(False)
-        ax.spines["bottom"].set_visible(False)
-        ax.spines["right"].set_visible(False)
-        ax.spines["left"].set_visible(False)
-        ax.tick_params(axis="y", direction="in", length=10)
-        ax.tick_params(axis="x", direction="in", length=10)
-    cax = fig.add_axes([0.95, 0.25, 0.05, 0.5])
-    cbar = fig.colorbar(im, cax=cax, orientation="vertical")
-    cbar.ax.set_ylabel("Reflectivity")
-    plt.show()
+    im = ax.pcolor(X, Y, values, cmap="Reds_r", vmin=0.5, vmax=1)
+    ax.plot(wavevector, wavevector, "r-")
+    ax.set_ylim([min(wavenumber), max(wavenumber)])
+    ax.set_xlabel("Rescaled in-plane-wavevector $K = k_x c/\omega_{\mathrm{L}1}$")
+    ax.set_ylabel("Rescaled frequency $\Omega = \omega/\omega_{\mathrm{L}1}$")
+    ax.set_title("Reflectance")
+    ax.spines["top"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.tick_params(axis="y", direction="in", length=10)
+    ax.tick_params(axis="x", direction="in", length=10)
+    if cax is True:
+        cax = fig.add_axes([0.95, 0.15, 0.01, 0.7])
+        cbar = fig.colorbar(im, cax=cax, orientation="vertical")
+        cbar.ax.set_ylabel("Reflectivity")
